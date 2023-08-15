c63b1c27c6fb1e009a011d8268da952bb02deeb1
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2023-08-15 20:51:09 -0300

    Att repositório do Curso de A.D.S com a pasta da cadeira de Algoritimos e Programação

---
+++ Algoritimos_Programação/Alg_Python/Aula03.py
@@ -0,0 +1,24 @@
+print("Hello world!") 
+
+# Comando PRINT mostra algo na tela;
+# VARIÁVEIS são nomes que guardam um dado na memória;
+# Váriaveis do tipo BOOLEANO (BOOL)repesentan Verdadeiro (TRUE)ou Falso (FALSE)
+peso = 83.1 # variável FLOAT
+altura = 1.63 #variável FLOAT
+nome = "Luan Hoffmann"  # Váriavel STRING
+imc = peso/(altura*altura)
+print(imc)
+
+# Para obter um dado do TECLADO usamos a variavel IMPUT
+peso = input("Dígite o seu peso:")
+altura = input("Dígite sua alltura:")
+nome = input("Dígite seu nome:")
+imc = peso/(altura*altura)
+print(imc)
+
+# Para converter uma Str para Int dou Float, usamos os mesmo entre parenteses dentro da váriavel antes do input;
+peso = (int) input("Dígite o seu peso:")
+altura = (float) input("Dígite sua alltura:")
+nome = input("Dígite seu nome:")
+imc = peso/(altura*altura)
+print(imc)
