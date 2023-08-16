39df3627c015531735d6e00c668837d4c1771cbb
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2023-08-15 21:46:37 -0300

    TDE Algoritimo em phyton aula 03

---
+++ Algoritimos_Programação/Alg_Python/TDE_Aula03/02_Area_Retangulo.py
@@ -0,0 +1,3 @@
+#2. Área de um Retângulo: Peça ao usuário para fornecer o comprimento e a largura de um retângulo. Use a fórmula Área = comprimento * largura para determinar a área e apresente o valor.
+
+comprimento = float (input("Dígite o comprimento deseja do "))
