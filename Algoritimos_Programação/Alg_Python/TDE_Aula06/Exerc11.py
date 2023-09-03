a89affa3fb98cd42c93142081b70ee46f0f9e2fd
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2023-09-03 10:59:10 -0300

    att exerc 11, TDE 06 finalizada

--- Algoritimos_Programação/Alg_Python/TDE_Aula06/Exerc11.py
+++ Algoritimos_Programação/Alg_Python/TDE_Aula06/Exerc11.py
@@ -1,2 +1,9 @@
 #Verificar se um número é um quadrado perfeito.
 
+numero = int(input("Informe o numero desejado: \n"))
+raiz = numero ** (1/2)
+print(raiz)
+if ((raiz ** 2) == numero):
+    print(f"O número {numero} é um quadrado perfeito")
+else:
+    print(f"O número {numero} não é um quadrado perfeito")
