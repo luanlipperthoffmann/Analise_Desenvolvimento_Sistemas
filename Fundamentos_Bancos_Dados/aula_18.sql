c691615fcf2f23ae35da58dfc00102d3f2fc54a2
luanlipperthoffmann <you@example.comluan.lippert91@gmail.com>
2023-11-13 21:19:01 -0300

    TDE 18

--- Fundamentos_Bancos_Dados/aula_18.sql
+++ Fundamentos_Bancos_Dados/aula_18.sql
@@ -33,3 +33,72 @@ CREATE TABLE Enderecos
         Constraint Clientes_tem_Enderecos
             FOREIGN Key (Id_Cliente) references Clientes(Id)
 );
+
+ALTER	TABLE Clientes
+	add Telefone varchar(50)
+
+ALTER  TABLE  Enderecos
+    DROP COLUMN E-mail varchar (50) 
+
+
+ALTER TABLE Enderecos
+	DROP COLUMN Email
+
+ALTER TABLE Enderecos
+	CHANGE COLUMN UF Uf char (2)
+
+ALTER TABLE Enderecos
+DROP CONSTRAINT Clientes_tem_Enderecos
+
+ALTER TABLE Enderecos
+add CONSTRAINT Nova_fk FOREIGN KEY (id_cliente) REFERENCES Clientes(id)
+
+
+
+========
+--Inserção em tabela
+
+
+CREATE TABLE Produtos
+(
+    Codigo int Not Null Primary Key,
+    Descricao varchar (150),
+    Data_Cadastro Date,
+    Valor_Unitario float,
+    Cod_Cat int Not Null,
+    	Constraint Categoria_tem_Produtos
+        	Foreign Key (Cod_Cat) references Categorias(Codigo)
+);
+
+
+CREATE TABLE Produtos
+(
+    Codigo int Not Null Primary Key,
+    Descricao varchar (150),
+    Data_Cadastro Date,
+    Valor_Unitario float,
+    Cod_Cat int Not Null,
+    	Constraint Categoria_tem_Produtos
+        	Foreign Key (Cod_Cat) references Categorias(Codigo)
+);
+
+
+CREATE TABLE Fornecedores
+(
+    Codigo int Not Null Primary Key,
+    Nome varchar(100)
+);
+
+
+CREATE TABLE Pedidos
+(
+    Quantidade int Not Null Primary Key,
+    Valor_Unitario float,
+    Data Date,
+    Cod_Fornec int Not Null,
+    Prod_Cod int Not Null,
+    Constraint Fornecedores_Pede_Produtos
+        Foreign Key (Cod_Fornec) references Fornecedores(Codigo),
+    Constraint Produtos_tem_Fornecedores
+        Foreign Key (Prod_Cod)references Produtos(Codigo)
+);
