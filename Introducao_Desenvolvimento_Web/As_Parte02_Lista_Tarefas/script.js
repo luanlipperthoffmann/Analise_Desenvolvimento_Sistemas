3ac2511606d20b03ad6be5e59718727cd24166ae
luanlipperthoffmann <you@example.comluan.lippert91@gmail.com>
2023-12-02 21:10:28 -0300

    tde AS I.D.W finalizada

--- Introducao_Desenvolvimento_Web/As_Parte02_Lista_Tarefas/script.js
+++ Introducao_Desenvolvimento_Web/As_Parte02_Lista_Tarefas/script.js
@@ -1,7 +1,48 @@
 function adicionar(){
-    const nomeTime = document.getElementById('time').value
+    const nomeTarefa = document.getElementById('tarefa').value
     const li = document.createElement('li');
-    li.innerText = nomeTime
-    const listaTimes = document.getElementById('tabelaTimes')
-    listaTimes.appendChild(li)
+    li.innerText = ' '
+
+    const btnRemover = document.createElement('button');
+    btnRemover.type = 'button';
+    btnRemover.innerText = 'Remover';
+    btnRemover.addEventListener('click', function() {li.remove()})
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('click', function(){
+        //ADICIONAR NO CONCLUIDOS.
+        const tarefasConcluidas = document.getElementById('tarefasConcluidas')
+        tarefasConcluidas.appendChild(li)
+        const date = new Date()
+        span.innerText +=  `    ${date.getHours()}:${date.getMinutes()}`
+        span = marcar(li)
+    })
+
+    const span = document.createElement('span' )
+    span.innerText = nomeTarefa
+
+    const div = document.createElement('div')
+
+    div.appendChild(checkbox)
+    div.appendChild(span)
+
+    li.appendChild(div)
+    li.appendChild(btnRemover)
+    
+    const tarefasPendentes = document.getElementById('tarefasPendentes')
+    tarefasPendentes.appendChild(li)
+}
+
+function mudaDark(){
+    const body = document.querySelector('body')
+    body.classList.add('darkTheme')
+}
+function mudaLight(){
+    const body = document.querySelector('body')
+    body.classList.remove('darkTheme')
+}
+function marcar(){
+    const li = document.querySelector('li')
+    li.classList.toggle('selected')
 }
