8157b1af516db2641008cb9d39cbd25d1e245c98
luan.lipperthoffmann <luan.lippert91@rede.ulbra.br>
2024-04-07 16:09:03 -0300

    att da aula 6 e inicio da tde de laboratório de programação

---
+++ Laboratório_Programação/Aula06/Program.cs
@@ -0,0 +1,135 @@
+﻿/* if com string
+System.Console.WriteLine("### Instrução IF #####");
+
+System.Console.WriteLine("Cliente especial (S/N)");
+var resposta = Console.ReadLine().ToUpper();
+
+if (resposta == "S")
+{
+    System.Console.WriteLine("Desconto de 10%");
+}
+*/
+
+/* if com booblean, precisa converter a string pra boolean
+System.Console.WriteLine("### Instrução IF #####");
+
+System.Console.WriteLine("Cliente especial (S/N)");
+var resposta = Convert.ToBoolean(Console.ReadLine());
+
+if (resposta)
+{
+    System.Console.WriteLine("Desconto de 10%");
+}
+*/
+
+// // System.Console.WriteLine("Informe o valor de x:");
+// // int x = Convert.ToInt32(Console.ReadLine());
+
+// // System.Console.WriteLine("Informe o valor de y:");
+// // int y = Convert.ToInt32(Console.ReadLine());
+
+// // if (x>y)
+// // {
+// //     System.Console.WriteLine("x é maior que y");
+// // }
+// // if (x<y)
+// // {
+// //     System.Console.WriteLine("x é menor que y");
+// // }
+// // if (x==y)
+// // {
+// //     System.Console.WriteLine("x é igual a y");
+// // }
+
+// System.Console.WriteLine("### INSTRUÇÃO IF-ELSE");
+// System.Console.WriteLine("Informe a nota do aluno");
+// var nota = Convert.ToInt32(Console.ReadLine());
+// if(nota >5)
+// {
+//     System.Console.WriteLine("O aluno foi aprovado");
+// }
+// else
+// {
+//     System.Console.WriteLine("O aluno foi reprovado");
+// }
+
+/*
+System.Console.WriteLine("Informe o valor de x:");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Informe o valor de y:");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x>y)
+{
+System.Console.WriteLine("x é maior que y");
+}
+else
+{
+    if (x<y)
+    {
+    System.Console.WriteLine("x é menor que y");
+    }
+    if (x==y)
+    {
+    System.Console.WriteLine("x é igual a y");
+    }
+}
+*/
+
+/*
+System.Console.WriteLine("---ESTRUTURA SWICTH CASE------");
+int compra = 600;
+System.Console.WriteLine("Valor da compra = R$600,00");
+System.Console.WriteLine("Informe o número de parcelas: 1 á 3");
+var numParcelas= Convert.ToInt32(Console.ReadLine());
+switch(numParcelas)
+{
+    case 1:
+    System.Console.WriteLine($"Prestação R${compra/numParcelas}");
+    break;
+
+    case 2:
+    System.Console.WriteLine($"Prestação R${compra/numParcelas}");
+    break;
+
+    case 3:
+    System.Console.WriteLine($"Prestação R${compra/numParcelas}");
+    break;
+
+    default:
+    System.Console.WriteLine("Valor inválido, informe 1,2,3");
+    break;
+}
+*/
+
+/*
+System.Console.WriteLine("---- INSTRUÇÃO WHILE ------");
+int i =10;
+while(i<12)
+{
+    System.Console.WriteLine($"i = {i}");
+    i++;
+}
+*/
+
+/*
+System.Console.WriteLine("---- INSTRUÇÃO WHILE ------");
+int i =10;
+do
+{
+    System.Console.WriteLine($"i = {i}");
+    i++;
+}while(i<12);
+*/
+
+//CALCULADORA USANDO FOR
+/*
+int numero, resultado;
+System.Console.WriteLine("Informe um número inteiro:");
+numero = Convert.ToInt32(Console.ReadLine());
+for(int i =1; i<=10; i++)
+{
+    resultado = numero * i;
+    System.Console.WriteLine(numero + " X " + i + " = " + resultado);
+}
+*/
+
---
+++ Laboratório_Programação/Tde03/Program.cs
@@ -0,0 +1,137 @@
+﻿/*
+1 - Escreva um programa para receber 3 números inteiros e a seguir calcular e exibir qual deles é o maior
+Encontre o maior dentre 3 números:
+Primeiro Número : 65465
+Segundo Número : 64658;
+Terceiro Número : 65464
+O primeiro número : 65465 é o maior
+
+
+    int num1 = 65465;
+    int num2 = 64658;
+    int num3 = 65464;
+    if (num1 > num2 & num1 > num3)
+    {
+        System.Console.WriteLine($"O número 1 é o maior: {num1}");
+    } 
+    if (num2 > num1 & num2 > num3)
+    {
+        System.Console.WriteLine($"O número 2 é o maior: {num2}");
+    }
+    if (num3 > num1 & num3 > num2)
+    {
+        System.Console.WriteLine($"O número 3 é o maior: {num3}");
+    }
+*/
+
+
+/*
+2- Escreva um programa para exibir os 10 primeiros números naturais e calcular a sua soma usando os loop 
+while, do-while e for.
+Os 10 primeiros números naturais são :
+1 2 3 4 5 6 7 8 9 10
+A soma dos números é : 55
+
+
+    int i =1;
+    int soma = 0;
+    while (i>=1 && i<11)
+    {
+        System.Console.WriteLine(i);
+        soma += i;
+        i++;
+    } 
+    System.Console.WriteLine($"A soma dos números naturais é de: {soma}");
+*/
+
+
+/*
+3- Escreva um programa para exibir a tabela de multiplicação de um número natural maior que zero 
+recebido via teclado 
+- Verifique se o número é maior que zero e emita uma mensagem
+- Considere a tabela de multiplicação de 1 até 10
+- Após exibir a tabela torne a solicitar outro número 
+- Para sair do loop defina uma condição de saída 
+Dica: Use os loop while e for e para sair a instrução break;
+*/
+char continue;
+System.Console.WriteLine("Deseja realizar uma multiplicação?");
+continue = Convert.ToChar(Console.ReadLine().ToUpper());
+
+while (continue=="S")
+{
+    System.Console.WriteLine("Informe o numero a ser multiplicado:");
+    int multiplicador = Convert.ToInt32(Console.ReadLine());
+    if (multiplicador>0)
+    {
+        
+        for(int i=1; i<=10; i++)
+        {   
+            System.Console.WriteLine($"{multiplicador}x{i}= {multiplicador*i}"); 
+        }
+    } 
+    else 
+    {
+        System.Console.WriteLine("Número invalido!");
+    }
+}
+
+
+
+    
+
+/*
+4- Crie um programa para exbir no console os números pares de 10 a 20, ambos incluídos, exceto 16, de 3 
+maneiras diferentes:
+- Incrementando 2 em cada passo
+- Incrementando 1 em cada passo 
+- Com e loop infinito (use "break" e "continue")
+*/
+
+/*
+5 - Escreva um programa para calcular o fatorial de um número inteiro. 
+O fatorial de um número é representado por : n! => n * (n – 1) * (n – 2) ....2*1 
+Exemplo : fatorial de 6 é representado por 6! = 6*5*4*3*2*1     
+*/
+
+/*
+6 - Escreva um programa para exibir as tabelas de multiplicação do 2 ao 6 usando o loop do-while aninhado
+
+Modelo de saída
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
+*/
+
+/*
+7 - Crie um programa que recebe na entrada de dados um número inteiro de 0 a 10 que representa a nota de 
+um aluno. Com base na tabela a seguir imprima no console qual o resultado da avaliação do aluno. (Use a 
+instrução switch, break e default em um loop infinito e defina uma condição de saida.)
+VER IMAGEM NA TDE
+*/
+
+/*
+8 - Crie um programa para realizar as operações de adição, subtração, multiplicação e divisão de números 
+inteiros. (considere que na divisão podemos ter números fracionários e que não existe divisão por zero e 
+quando isso ocorrer exibir uma mensagem de alerta
+VER IMAGEM NA TDE
+*/
+
+
