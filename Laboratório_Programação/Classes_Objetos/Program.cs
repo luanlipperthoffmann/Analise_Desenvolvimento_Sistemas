b5eb718c6f2493c6ec47fd913eeeb4da849654f0
luanhoffmann <61809243+luanlipperthoffmann@users.noreply.github.com>
2024-04-18 21:04:00 -0300

    att da aula de classes  e objetos de L.p

---
+++ Laboratório_Programação/Classes_Objetos/Program.cs
@@ -0,0 +1,24 @@
+﻿//OBJETO é uma instancia de uma classe, criado pelo operador new, possui caracteristica proprias.
+//CLASSE é um modelo, hm planejamento, tal como a maquete de uma casa.
+int x =100;
+
+Pessoa p1 = new Pessoa();
+p1.nome = "Maria";
+p1.idade = 23;
+p1.sexo = "feminino";
+
+System.Console.WriteLine($"{p1.nome} {p1.idade} {p1.sexo}");
+
+Pessoa p2 =new Pessoa();
+p2.nome = "João";
+p2.idade = 30;
+p2.sexo = "masculino";
+
+System.Console.WriteLine($"{p2.nome} {p2.idade} {p2.sexo}");
+
+class Pessoa  //estrutura de um objeto
+{
+    public string nome;
+    public int idade;
+    public string sexo;
+}
--- Laboratório_Programação/Classes_Objetos/obj/Debug/net8.0/Classes_Objetos.AssemblyInfo.cs
+++ Laboratório_Programação/Classes_Objetos/obj/Debug/net8.0/Classes_Objetos.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Tde03")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Classes_Objetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7793eee762d202855e12122d948631ec64ca622")]
-[assembly: System.Reflection.AssemblyProductAttribute("Tde03")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Tde03")]
+[assembly: System.Reflection.AssemblyProductAttribute("Classes_Objetos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Classes_Objetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
