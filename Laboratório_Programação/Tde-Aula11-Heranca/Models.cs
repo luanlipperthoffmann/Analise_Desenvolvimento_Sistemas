bb70c541947e7a35a320edd093fb02e28e491830
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2024-05-31 14:08:50 -0300

    Tde-Aula11-Heranca Finalizada

---
+++ Laboratório_Programação/Tde-Aula11-Heranca/Models.cs
@@ -0,0 +1,241 @@
+//Exercício 1: Classe Base e Classe Derivada
+using System.Security.Cryptography.X509Certificates;
+
+public class Veiculo
+{
+    public string Marca;
+    public int Ano;
+
+    public Veiculo(string marca, int ano)
+    {
+        Marca = marca;
+        Ano = ano;
+    }
+}
+
+public class Carro : Veiculo
+{
+    public string Modelo;
+
+    public Carro(string marca, int ano, string modelo) : base(marca, ano) {
+        Modelo = modelo;
+    }
+
+    public void ExibirDetalhes()
+    {
+        System.Console.WriteLine($"Marca: {Marca}, Ano: {Ano}, Modelo: {Modelo}");
+    }
+}
+
+
+//Exercício 2: Sobrescrita de Método
+public class Animal
+{
+    public virtual void EmitirSom()
+    {
+        System.Console.WriteLine("O animal emite som");
+    }
+}
+
+public class Gato : Animal
+{
+    public override void EmitirSom()
+    {
+        System.Console.WriteLine("O gato mia!");
+    }
+}
+
+
+//Exercício 3: Modificadores de Acesso
+public class Pessoa
+{
+    private string? nome;
+    public string? Nome;
+
+    public void ExibirNome()
+    {
+        Nome = "Luan";
+        System.Console.WriteLine($"Nome: {Nome}");
+    }
+}
+
+public class Funcionario : Pessoa
+{
+    protected double Salario;
+
+    public void InformaSalario()
+    {   
+        System.Console.WriteLine("Informe o salario: ");
+        Salario = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+
+//Exercício 4: Construtores em Classes Derivadas
+public class Produto
+{
+    public string Nome;
+    public decimal Preco;
+
+    public Produto()
+    {
+        Nome = "Notbook";
+        Preco = 3000;
+    }
+}
+
+public class Eletronico : Produto
+{
+    public string Garantia;
+
+    public Eletronico()
+    {
+        Garantia = "1 Ano";
+        System.Console.WriteLine($"Nome: {Nome}, Preço: {Preco}, Garantia: {Garantia}");
+    }
+}
+
+
+//Exercício 5: Polimorfismo com Herança
+public class Forma
+{
+    public virtual double CalcularArea()
+    {
+        return 0;
+    }
+}
+
+public class Quadrado : Forma
+{   
+    public double lado;
+
+    public Quadrado(double lado)
+    {
+        this.lado = lado;
+    }
+    public override double CalcularArea()
+    {
+        return lado*lado;
+    }
+}
+
+public class Circulo : Forma
+{   
+    public double raio;
+
+    public Circulo(double raio)
+    {
+        this.raio = raio;
+    }
+    public override double CalcularArea()
+    {
+        return raio*3.14;
+    }
+}
+
+
+//Exercício 6: Encapsulamento com Propriedades
+public class ContaBancaria
+{
+    public string NumeroConta;
+    public string Titular;
+    private double Saldo;
+
+    public ContaBancaria(string NumeroConta, string Titular, double Saldo)
+    {
+        this.NumeroConta = NumeroConta;
+        this.Titular = Titular;
+        this.Saldo = Saldo;
+    }
+}
+
+
+//Exercício 7: Herança e Sobrescrita
+public class Funcionario07
+{
+    public string? Nome;
+    public int Bonus;
+
+    public virtual int CalcularBonus()
+    {
+        return 0;
+    }
+}
+
+public class Gerente : Funcionario07
+{
+    public int MaiorBonus;
+
+    public Gerente(int MaiorBonus)
+    {
+        this.MaiorBonus = MaiorBonus;
+    }
+}
+
+
+//Exercício 8: Usando Protected
+public class Banco
+{
+    protected string? Nome;
+}
+
+public class Agencia : Banco
+{
+    public Agencia(string Nome)
+    {
+        this.Nome = Nome;
+    }
+    public void ExibiNome()
+    {
+        System.Console.WriteLine($"Nome do Banco: {Nome}");
+    }
+}
+
+
+//Exercício 9: Herança Múltipla com Interfaces
+public interface Imprimivel
+{
+    void Imprimir();
+}
+
+public class Documento : Imprimivel
+{
+    public virtual void Imprimir()
+    {
+        System.Console.WriteLine("Documento sendo impresso");
+    }
+}
+
+public class Relatorio : Documento
+{
+    public override void Imprimir()
+    {
+        System.Console.WriteLine("Relatorio sendo impresso");
+    }
+}
+
+
+//Exercício 10: Combinando Herança e Polimorfismo
+public class Funcionario10
+{
+    public virtual void Trabalhar()
+    {
+        return;
+    }
+}
+
+public class Desenvolvedor : Funcionario10
+{
+    public override void Trabalhar()
+    {
+        System.Console.WriteLine("Desenvolvedor toma café e passa raiva!");
+    }
+}
+
+public class Designer : Funcionario10
+{
+    public override void Trabalhar()
+    {
+        System.Console.WriteLine("Designer toma toddy e muda cor de botão!");
+    }
+}
---
+++ Laboratório_Programação/Tde-Aula11-Heranca/Program.cs
@@ -0,0 +1,114 @@
+﻿/*
+Exercício 1: Classe Base e Classe Derivada
+Crie uma classe base Veiculo com as propriedades Marca e Ano. Crie uma classe derivada Carro que adicione a propriedade Modelo e um método ExibirDetalhes que exibe todas as informações do carro.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 01:"); //este CW serve só pra diferenciar os  prints por ordem de exercício
+
+Carro carro = new Carro("Citroen", 2011, "C4");
+carro.ExibirDetalhes();
+
+
+/*
+Exercício 2: Sobrescrita de Método
+Crie uma classe base Animal com um método virtual EmitirSom. Crie uma classe derivada Gato que sobrescreva o método EmitirSom para exibir "O gato mia".
+*/
+System.Console.WriteLine("\nEXERCÍCIO 02");
+
+Animal animal = new Animal();
+animal.EmitirSom();
+
+Gato gato = new Gato();
+gato.EmitirSom();
+
+
+/*
+Exercício 3: Modificadores de Acesso
+Crie uma classe Pessoa com um campo privado nome e uma propriedade pública Nome. Adicione um método ExibirNome que exibe o nome da pessoa. Crie uma classe derivada Funcionario que adicione um campo protegido salario e um método para definir o salário.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 03");
+
+var funcionario = new Funcionario();
+funcionario.ExibirNome();
+funcionario.InformaSalario();
+
+/*
+Exercício 4: Construtores em Classes Derivadas
+Crie uma classe base Produto com um construtor que aceita nome e preco. Crie uma classe derivada Eletronico que adicione a propriedade Garantia e um construtor que aceite nome, preco e garantia.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 04");
+
+Produto produto = new Produto();
+Eletronico eletronico = new Eletronico();
+
+
+/*
+Exercício 5: Polimorfismo com Herança
+Crie uma classe base Forma com um método virtual CalcularArea. Crie classes derivadas Quadrado e Circulo que sobrescrevam o método CalcularArea para calcular a área específica de cada forma.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 05");
+
+Quadrado quadrado = new Quadrado(5.2);
+System.Console.WriteLine(quadrado.CalcularArea());
+
+var circulo = new Circulo(8.4);
+System.Console.WriteLine(circulo.CalcularArea());
+
+
+/*
+Exercício 6: Encapsulamento com Propriedades
+Crie uma classe ContaBancaria com propriedades NumeroConta, Titular e Saldo. Use modificadores de acesso para garantir que Saldo só possa ser modificado dentro da classe.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 06");
+
+ContaBancaria contaBancaria = new ContaBancaria("12345-70", "Luan", 10100);
+System.Console.WriteLine($"Conta Bancaria: {contaBancaria.NumeroConta}");
+System.Console.WriteLine($"Titular: {contaBancaria.Titular}");
+
+
+/*
+Exercício 7: Herança e Sobrescrita
+Crie uma classe Funcionario com uma propriedade Nome e um método virtual CalcularBonus. Crie uma classe derivada Gerente que sobrescreva CalcularBonus para retornar um valor maior de bônus.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 07");
+
+Funcionario07 funcionario07 = new Funcionario07();
+System.Console.WriteLine($"O valor do bonus original é de: {funcionario07.Bonus}");
+Gerente gerente = new Gerente(10);
+System.Console.WriteLine($"O novo valor do Bonus é de: {gerente.MaiorBonus}");
+
+
+/*
+Exercício 8: Usando Protected
+Crie uma classe Banco com uma propriedade protegida Nome. Crie uma classe derivada Agencia que tenha um método para definir o nome do banco.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 08");
+
+Banco banco =new Banco();
+Agencia agencia = new Agencia("Bradesco");
+agencia.ExibiNome();
+
+
+/*
+Exercício 9: Herança Múltipla com Interfaces
+Crie uma interface IImprimivel com um método Imprimir. Crie uma classe Documento que implemente essa interface e uma classe Relatorio que herde de Documento e sobrescreva o método Imprimir.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 09");
+
+Documento documento = new Documento();
+documento.Imprimir();
+
+Relatorio relatorio = new Relatorio();
+relatorio.Imprimir();
+
+
+/*
+Exercício 10: Combinando Herança e Polimorfismo
+Crie uma classe base Funcionario com um método virtual Trabalhar. Crie classes derivadas Desenvolvedor e Designer que sobrescrevam o método Trabalhar para descrever suas atividades específicas.
+*/
+System.Console.WriteLine("\nEXERCÍCIO 10");
+
+Desenvolvedor desenvolvedor = new Desenvolvedor();
+desenvolvedor.Trabalhar();
+
+Designer designer = new Designer();
+designer.Trabalhar();
